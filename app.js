2e47c99c1b1263e26e5121182479047264eb8f19
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2023-03-09 11:40:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+// const fs = require("fs");
+const app = express();
+const port = 8000;
+
+app.use('/static', express.static('static'))
+app.use(express.urlencoded())
+
+
+// THIS IS USED FOR PUG FILES INCLUSION IN EXPRESS
+// app.set('view engine', 'pug')
+// app.set('views', path.join(__dirname, 'views'))
+
+// app.get('/', (req, res)=>{
+//     // const con = "This is the best Dance Academy to broaden up your skills."
+//     // const params = {'title': 'Dance Academy', 'content': con}
+//     res.status(200).render('index.pug')
+// })
+
+// THIS IS USED FOR HTML FILES INCLUSION IN EXPRESS
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+  });
+
+app.listen(port, ()=>{
+    console.log(`This application started successfully on port ${port}`);
+});
