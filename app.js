30ac0d4867536fe06539bd49bc49e7ccc42fcb6f
yugank <yugankbansal.00001276033.aesl@gmail.com>
2023-06-03 11:55:28 +0530

    files update

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
-// const fs = require("fs");
+const bodyParser=require("body-parser")
+const fs = require('fs');
 const app = express();
 const port = 80;
 
-app.use('/static', express.static('static'))
-app.use(express.urlencoded())
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 
 // THIS IS USED FOR PUG FILES INCLUSION IN EXPRESS
@@ -19,11 +23,53 @@ app.use(express.urlencoded())
 // })
 
 // THIS IS USED FOR HTML FILES INCLUSION IN EXPRESS
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/index.html'));
-    // res.sendFile(path.join(__dirname, '/views/index.html'));
-  });
+app.get("/", (req, res) => {
+ 
+  res.sendFile(path.join(__dirname, "./index.html"));
+  // res.sendFile(path.join(__dirname, '/views/index.html'));
+});
 
-app.listen(port, ()=>{
-    console.log(`This application started successfully on port ${port}`);
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+  // res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/contact.html"));
+
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+
+});
+
+app.get("/service", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/services.html"));
+
+});
+
+app.post('/contact',function (req, res) {
+  let name=req.body.name;
+  let phone=req.body.phone;
+  let email=req.body.email;
+  let address=req.body.address;
+  let desc=req.body.desc;
+  let js=[name,phone,email,address,desc]
+  const jsonString = JSON.stringify(js);
+
+  fs.appendFile('./input.txt', jsonString, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Text successfully written to file.');
+    }
+  });
+  console.log(js)
+
+    return res.send('Input saved successfully');
+})
+
+app.listen(port, () => {
+  console.log(`This application started successfully on port localhost:${port}`);
 });
